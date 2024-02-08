1fc27d0a23fe0ea20a4cc59bde180d1b6e5e4d40
notsunshine <alexsupper75@gmail.com>
2024-02-09 00:26:12 +0300

    add web security config

---
+++ src/main/java/io/svinoczar/api/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package io.svinoczar.api.config;
+
+import io.svinoczar.api.security.AuthenticationManager;
+import io.svinoczar.api.security.BearerTokenServerAuthenticationConverter;
+import io.svinoczar.api.security.JwtHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+@EnableReactiveMethodSecurity
+public class WebSecurityConfig {
+    @Value("${jwt.secret")
+    private String secret;
+
+    private final String[] publicRoutes = {"/api/v1/auth/register", "/api/v1/auth/login"};
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationManager authManager) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers(HttpMethod.OPTIONS)
+                        .permitAll()
+                        .pathMatchers(publicRoutes)
+                        .permitAll()
+                        .anyExchange().authenticated()
+                )
+                .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec
+                        .authenticationEntryPoint((swe, e) -> {
+                            log.error("Unauthorized error in `securityWebFilterChain`: {}", e.getMessage());
+                            return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED));
+                        }).accessDeniedHandler((swe, e) -> {
+                            log.error("Access denied in `securityWebFilterChain`: {}", e.getMessage());
+                            return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN));
+                        }))
+                .addFilterAt(bearerAuthenticationFilter(authManager), SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+    }
+
+    private AuthenticationWebFilter bearerAuthenticationFilter(AuthenticationManager authenticationManager) {
+        AuthenticationWebFilter bearerAuthenticationFilter = new AuthenticationWebFilter(authenticationManager);
+        bearerAuthenticationFilter.setServerAuthenticationConverter(new BearerTokenServerAuthenticationConverter(new JwtHandler(secret)));
+        bearerAuthenticationFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/**"));
+
+        return bearerAuthenticationFilter;
+    }
+}
